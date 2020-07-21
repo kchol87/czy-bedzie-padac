04bdc1cbdafe5abf46d13be8a235b051958b27bc
bitkiwolowe87 <68591082+bitkiwolowe87@users.noreply.github.com>
2020-07-21 22:19:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+src="https://antistorm.eu/api-v1/antistormWidgetV2.js" 
+charset="UTF-8"
+    var widget = new antistormWidget(); 
+    widget.url="https://antistorm.eu/webservice.php";
+    widget.id="390";
+    widget.pytajSerwer();
