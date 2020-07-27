8197a525fe700e716027a039387979abc0727e8e
bitkiwolowe87 <kcholerzynski@gmail.com>
2020-07-27 22:35:14 +0200

    delete script

--- script.js
+++
@@ -1,6 +0,0 @@
-src="https://antistorm.eu/api-v1/antistormWidgetV2.js" 
-charset="UTF-8"
-    var widget = new antistormWidget(); 
-    widget.url="https://antistorm.eu/webservice.php";
-    widget.id="390";
-    widget.pytajSerwer();
