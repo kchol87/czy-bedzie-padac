bb6007bb2cf0ac9069dd9146b223434a6658d5f8
kchol87 <68591082+kchol87@users.noreply.github.com>
2020-08-05 13:08:43 +0200

    some cleaning up

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
 /*Creates a limited input city searchbox with hints*/
-
-
 var cityList = [];
 let URL = new Request("cities.json");
 fetch(URL)
 .then(response => response.json())
 .then(data => cityList = data);
 const myList = document.getElementById("myList");
-/*for (const [key, value] of Object.entries(cityList)) {myList.add(new Option(key, value))};*/
-
 for (i = 0; i < 439; i++) {myList.insertAdjacentHTML('beforeend', '<option value="'+ Object.keys(cityList)[i] + '">')};
 
-/*document.addEventListener("click", function() {
-    for (const [key, value] of Object.entries(cityList))
-    {myList.add(new Option(key, value))};
-    }
-);*/
-
-
-
 /* Makes the search button inactive if a valid city input is not chosen*/
-const checkButton = document.querySelector("#checkButton");
-(document.querySelector("#myList") == null) ? checkButton.disabled = true : checkButton.disabled = false;
-
 
 /* If a valid city is chosen in the searchbox and button is clicked, combines city name with city number 'cityNumber'. */
 
-
-
 /*Gets weather data based on city from Antistorm.eu API and save it to 'weatherData' variable*/
-/*let weatherData;
+let weatherData;
 fetch('http://antistorm.eu/webservice.php?id=' + cityNumber)
     .then(response => response.json())
     .then(data  => weatherData = data);
