0d0658364318fc4c23ea1c4b2a7afd0dd5718776
kchol87 <68591082+kchol87@users.noreply.github.com>
2020-08-05 15:46:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-/*Creates a limited input city searchbox with hints*/
+/* Fetches city list from local file*/
 var cityList = [];
 let URL = new Request("cities.json");
 fetch(URL)
 .then(response => response.json())
 .then(data => cityList = data);
+
+/* Creates city option entries in form*/
 const myList = document.getElementById("myList");
-for (i = 0; i < 439; i++) {myList.insertAdjacentHTML('beforeend', '<option value="'+ Object.keys(cityList)[i] + '">')};
+for (i = 0; i < 8; i++) {myList.insertAdjacentHTML('beforeend', '<option value="'+ cityList[i] + '">')};
+
+/*Function to get weather data from antistorm.eu*/
+const getWeatherData = function() {
+  let inputCityName = document.getElementById("inputCity").value;
+  let cityNumber = cityList.indexOf(inputCityName);
+  let weatherData;
+  fetch('http://antistorm.eu/webservice.php?id=' + cityNumber)
+    .then(response => response.json())
+    .then(data  => weatherData = data);
+ return weatherData;
+ };
+
+/*const getWeather() = function() {
+  (document.getElementById("inputCity") !== null) ? getWeatherData() : console.log("ERROR empty city");
+};*/
+
+const button = document.getElementById("button");
+button.addEventListener("click", getWeatherData());
 
 /* Makes the search button inactive if a valid city input is not chosen*/
 
 /* If a valid city is chosen in the searchbox and button is clicked, combines city name with city number 'cityNumber'. */
 
 /*Gets weather data based on city from Antistorm.eu API and save it to 'weatherData' variable*/
-let weatherData;
+/*let weatherData;
 fetch('http://antistorm.eu/webservice.php?id=' + cityNumber)
     .then(response => response.json())
-    .then(data  => weatherData = data);
+    .then(data  => weatherData = data);*/
 
 /* Changes the index.html according to weather data*/
 
