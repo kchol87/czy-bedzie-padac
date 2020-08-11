b09ab94b61afd284804221ce9903781f3bb30e2a
bitkiwolowe87 <kcholerzynski@gmail.com>
2020-08-11 23:58:12 +0200

    cleaning the js code

--- script.js
+++ script.js
@@ -1,80 +1,81 @@
-const myList = document.getElementById("myList");
-/* Creates city option entries in form*/
-for (i=0; i < 439; i++) {
-    myList.insertAdjacentHTML('beforeend', '<option value="' + cityList[i] + '">')};
+const myList = document.querySelector("#myList"); 
+
+const insertCityList = function(cityList) {
+    for (const city of cityList) {
+        myList.insertAdjacentHTML('beforeend', '<option value="' + city + '" id="'+cityList.indexOf(city)+'">')}
+};
+
+fetch('cities.json')
+.then((resp) => resp.json())
+.then(data => insertCityList(data.cities));
 
 /*Function to get weather data from antistorm.eu*/
 
-const button = document.getElementById('button');
-const header = document.getElementById("header");
-const img = document.getElementById("weatherICO");
-const text = document.getElementById("text");
-const form = document.getElementById("form");
+const button = document.querySelector(".button-check");
+const header = document.querySelector(".header");
+const img = document.querySelector(".weatherICO");
+const text = document.querySelector(".text");
+const form = document.querySelector('[class = "form"]');
 const rainICO = '<div class="icon rainy"><div class="cloud"></div><div class="rain"></div></div>';
 const norainICO = '<div class="icon cloudy"><div class="cloud"></div><div class="cloud"></div></div>';
 const stormICO = '<div class="icon thunder-storm"><div class="cloud"></div><div class="lightning"><div class="bolt"></div><div class="bolt"></div></div></div>';
-let background = document.body.style.backgroundImage;
+const background = document.querySelector("body").style.backgroundImage;
+};
+
+
+const doTheWork = function(weatherData) {
+    const city = weatherData.m;
+    const rainAlarm = weatherData.a_o;
+    const rainComing = weatherData.t_o;
+    const rainProbs = weatherData.p_o;
+    const stormAlarm = weatherData.s;
+    const stormComing = weatherData.t_b;
+    const stormProbs = weatherData.p_b;
+    const itWillRain = function() {
+        header.innerHTML = city;
+        const rainProbsResult = function() {
+            const result =  (rainProbs > 30) ? 'wysokie' : (rainProbs > 10) ? 'znaczne' : "niskie";
+            return result};
+        text.innerHTML = '<e>Będzie padać za ' + rainComing + ' minut.</e><br>Prawdpodobieństwo opadów jest ' + rainProbsResult()  +'.';
+        img.innerHTML = rainICO;
+        form.remove();
+    };
+    const itWontRain = function() {
+        header.innerHTML = city;
+        text.innerHTML = 'Nie będzie padać.';
+        img.innerHTML = norainICO;
+        form.remove();
+    };
+    const itWillRainStorm = function() {
+        header.innerHTML = city;
+        const stormProbsResult = function() {
+            const result =  (stormProbs > 30) ? 'wysokie' : (stormProbs > 10) ? 'znaczne' : "niskie";
+            return result};
+        text.innerHTML = 'Będzie grzmieć za ' + stormComing + 'minut. Prawdpodobieństwo burzy jest ' + stormProbsResult() +'.';
+        img.innerHTML = stormICO;
+        form.remove();
+    };
+    const itRains = function() {
+        header.innerHTML = city;
+        text.innerHTML = 'Właśnie pada.';
+        img.innerHTML = rainICO;
+        form.remove();
+    };
+    const itStorm = function() {
+        header.innerHTML = city;
+        text.innerHTML = 'Trwa burza.';
+        img.innerHTML = stormICO;
+        form.remove();
+    };
+    (stormAlarm == 1) ? itStorm() : (rainAlarm == 1) ? itRains() : (stormProbs !== 0) ? itWillRainStorm() : (rainProbs !== 0) ? itWillRain() : itWontRain();
+}
 
 button.addEventListener("click", function(event) {
     event.preventDefault();
-    let inputCityName = document.getElementById("inputCity").value;
-    let cityNumber = cityList.indexOf(inputCityName);
-    fetch('https://antistorm.eu/webservice.php?id='+cityNumber)  
+    const cityNumber = document.querySelector('[value="'+document.querySelector(".inputCity").value+'"]').id;
+    fetch('https://antistorm.eu/webservice.php?id=' + cityNumber)  
     .then((resp) => resp.json())
-    .then(data => {
-        const city = data.m;
-        const rainAlarm = data.a_o;
-        const rainComing = data.t_o;
-        const rainProbs = data.p_o;
-        const stormAlarm = data.s;
-        const stormComing = data.t_b;
-        const stormProbs = data.p_b;
-        const itWillRain = function() {
-            header.innerHTML = city;
-            let rainProbsResult = function() {
-                let result =  (rainProbs > 30) ? 'wysokie' : (rainProbs > 10) ? 'znaczne' : "niskie";
-                return result};
-            text.innerHTML = '<e>Będzie padać za ' + rainComing + ' minut.</e><br>Prawdpodobieństwo opadów jest ' + rainProbsResult()  +'.';
-            img.innerHTML = rainICO;
-            button.remove();
-            form.remove();
-            background = 'url("./wall/rain.jpg")';
-        };
-        let itWontRain = function() {
-            header.innerHTML = city;
-            text.innerHTML = 'Nie będzie padać.';
-            img.innerHTML = norainICO;
-            button.remove();
-            form.remove();
-            background = 'url("./wall/norain.jpg")';
-        };
-        let itWillRainStorm = function() {
-            header.innerHTML = city;
-            let stormProbsResult = function() {
-                let result =  (stormProbs > 30) ? 'wysokie' : (stormProbs > 10) ? 'znaczne' : "niskie";
-                return result};
-            text.innerHTML = 'Będzie grzmieć za ' + stormComing + 'minut. Prawdpodobieństwo burzy jest ' + stormProbsResult() +'.';
-            img.innerHTML = stormICO;
-            button.remove();
-            form.remove();
-            background = 'url("./wall/storm.jpg")';
-        };
-        let itRains = function() {
-            header.innerHTML = city;
-            text.innerHTML = 'Właśnie pada.';
-            img.innerHTML = rainICO;
-            button.remove();
-            form.remove();
-            background = 'url("./wall/rain.jpg")';
-        };
-        let itStorm = function() {
-            header.innerHTML = city;
-            text.innerHTML = 'Trwa burza.';
-            img.innerHTML = stormICO;
-            button.remove();
-            form.remove();
-            background = 'url("./wall/storm.jpg")';
-        };
-        (stormAlarm == 1) ? itStorm() : (rainAlarm == 1) ? itRains() : (stormProbs !== 0) ? itWillRainStorm() : (rainProbs !== 0) ? itWillRain() : itWontRain();
-    });
+    .then(data => { 
+        doTheWork(data)
+    })
 });
