ad68324478286e6b437b604292c13d9fea791d96
kchol87 <kcholerzynski@gmail.com>
2020-09-01 21:26:32 +0200

    changes to script.js (arrow functions mostly) + .gitignore

--- script.js
+++ script.js
@@ -1,45 +1,47 @@
-const myList = document.querySelector("#myList"); 
-const button = document.querySelector(".button-check");
-const header = document.querySelector(".header");
-const img = document.querySelector(".weatherICO");
-const text = document.querySelector(".text");
-const form = document.querySelector('[class = "form"]');
-const rainICO = '<div class="icon rainy"><div class="cloud"></div><div class="rain"></div></div>';
-const norainICO = '<div class="icon cloudy"><div class="cloud"></div><div class="cloud"></div></div>';
-const stormICO = '<div class="icon thunder-storm"><div class="cloud"></div><div class="lightning"><div class="bolt"></div><div class="bolt"></div></div></div>';
+const myList = document.querySelector("#myList")
+const button = document.querySelector(".button-check")
+const header = document.querySelector(".header")
+const img = document.querySelector(".weatherICO")
+const text = document.querySelector(".text")
+const form = document.querySelector('[class = "form"]')
+const rainICO = '<div class="icon rainy"><div class="cloud"></div><div class="rain"></div></div>'
+const norainICO = '<div class="icon cloudy"><div class="cloud"></div><div class="cloud"></div></div>'
+const stormICO = '<div class="icon thunder-storm"><div class="cloud"></div><div class="lightning"><div class="bolt"></div><div class="bolt"></div></div></div>'
 
-const insertCityList = function(cityList) {
-    for (const city of cityList) {
-        myList.insertAdjacentHTML('beforeend', '<option value="' + city + '" id="'+cityList.indexOf(city)+'">')}
-};
+const insertCityList = (cityList) => {
+  for (const city of cityList) {
+    myList.insertAdjacentHTML('beforeend', '<option value="' + city + '" id="' + cityList.indexOf(city) + '">')
+  }
+}
 
-const doTheWork = function(weatherData) {
-    const city = weatherData.m;
-    const rainAlarm = weatherData.a_o;
-    const rainComing = weatherData.t_o;
-    const rainProbs = weatherData.p_o;
-    const stormAlarm = weatherData.s;
-    const stormComing = weatherData.t_b;
-    const stormProbs = weatherData.p_b;
-    const itWillRain = function() {
-        header.innerHTML = city;
-        const rainProbsResult = function() {
-            const result =  (rainProbs > 30) ? 'wysokie' : (rainProbs > 10) ? 'znaczne' : "niskie";
-            return result};
-        text.innerHTML = '<e>Będzie padać za ' + rainComing + ' minut.</e><br>Prawdpodobieństwo opadów jest ' + rainProbsResult()  +'.';
-        img.innerHTML = rainICO;
-        form.remove();
-    };
-    const itWontRain = function() {
+const doTheWork = (weatherData) => {
+  const city = weatherData.m
+  const rainAlarm = weatherData.a_o
+  const rainComing = weatherData.t_o
+  const rainProbs = weatherData.p_o
+  const stormAlarm = weatherData.s
+  const stormComing = weatherData.t_b
+  const stormProbs = weatherData.p_b
+  const itWillRain = function () {
+    header.innerHTML = city
+    const rainProbsResult = () => {
+      const result = (rainProbs > 30) ? 'wysokie' : (rainProbs > 10) ? 'znaczne' : 'niskie'
+      return result
+    }
+    text.innerHTML = '<e>Będzie padać za ' + rainComing + ' minut.</e><br>Prawdpodobieństwo opadów jest ' + rainProbsResult() + '.'
+    img.innerHTML = rainICO
+    form.remove()
+  }
+  const itWontRain = () => {
         header.innerHTML = city;
         text.innerHTML = 'Nie będzie padać.';
         img.innerHTML = norainICO;
         form.remove();
         document.body.setAttribute("style", "background-image: url('wall/norain.jpg')");
     };
-    const itWillRainStorm = function() {
+    const itWillRainStorm = () => {
         header.innerHTML = city;
-        const stormProbsResult = function() {
+        const stormProbsResult = () => {
             const result =  (stormProbs > 30) ? 'wysokie' : (stormProbs > 10) ? 'znaczne' : "niskie";
             return result};
         text.innerHTML = 'Będzie grzmieć za ' + stormComing + 'minut. Prawdpodobieństwo burzy jest ' + stormProbsResult() +'.';
@@ -47,13 +49,13 @@ const doTheWork = function(weatherData) {
         form.remove();
         document.body.setAttribute("style", "background-image: url('wall/storm.jpg')")
     };
-    const itRains = function() {
+    const itRains = () => {
         header.innerHTML = city;
         text.innerHTML = 'Właśnie pada.';
         img.innerHTML = rainICO;
         form.remove();
     };
-    const itStorm = function() {
+    const itStorm = () => {
         header.innerHTML = city;
         text.innerHTML = 'Trwa burza.';
         img.innerHTML = stormICO;
@@ -64,15 +66,15 @@ const doTheWork = function(weatherData) {
 }
 
 fetch('cities.json')
-.then((resp) => resp.json())
-.then(data => insertCityList(data.cities));
+  .then((resp) => resp.json())
+  .then(data => insertCityList(data.cities))
 
-button.addEventListener("click", function(event) {
-    event.preventDefault();
-    const cityNumber = document.querySelector('[value="'+document.querySelector(".inputCity").value+'"]').id;
-    fetch('https://antistorm.eu/webservice.php?id=' + cityNumber)  
+button.addEventListener('click', (event) => {
+    event.preventDefault()
+    const cityNumber = document.querySelector('[value="'+document.querySelector(".inputCity").value + '"]').id;
+    fetch('https://antistorm.eu/webservice.php?id=' + cityNumber)
     .then((resp) => resp.json())
-    .then(data => { 
+    .then(data => {
         doTheWork(data)
     })
-});
+})
